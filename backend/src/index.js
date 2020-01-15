94f632211941cf8c3fe734994e435f5945bc9372
Carlos Sudsilowsky <sudsilowsky@gmail.com>
2020-01-15 18:00:44 -0300

    Back e Web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors");
 const app = express();
 
 mongoose.connect(
@@ -8,6 +9,7 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
